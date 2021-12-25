9aea1fb2be46a134f0d42aa2bfe1aabfa26b071c
Solaimani <89968516+Solaman10@users.noreply.github.com>
2021-12-25 07:47:55 +0530

    Update main.c

--- Data Structures/1.Stack/1.Stack implmnt using Array/main.c
+++ Data Structures/1.Stack/1.Stack implmnt using Array/main.c
@@ -1,1 +1,86 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+#define MAX 3
+
+int STACK[MAX],TOP;
+	
+void Display(int []);
+void Insert(int [],int);
+void Delete(int []);
+
+int main()
+{
+    int item = 0,choice = 0,get=1;
+    TOP =-1;
+		
+     if(get==1)
+	 printf("1.Display\n2.Insert\n3.Delete\n4.Exit\nEnter the choice you want: ");
+	 scanf("%d",&choice);
+	 get--;
+    {
+	 switch(choice)
+	 {
+	 	case 1:
+	 		Display(STACK);
+	 		break;
+	 	case 2:
+	 		printf("Enter element to be insert: ");
+	 		scanf("%d",&item);
+	 		Insert(STACK,item);
+	 		break;
+	 	case 3:
+	 		Delete(STACK);
+	 		break;
+	 	case 4:
+	 		exit(0);
+	 		break;
+	 	default:
+		    printf("Enter valid choice...");
+		    break;
+     }
+    } 
+    return 0;
+}
+
+void Display(int stack[])
+{
+	if(TOP==-1)
+	{
+		printf("The stack is empty(underflow)...");
+		return;
+	}
+	
+	printf("%d <-- Top",stack[TOP]);
+	for(int i=TOP-1;i>=0;i--)
+	{
+	 printf("%d ",stack[i]);
+    }
+    printf("\n\n");
+}
+
+void Insert(int stack[],int item)
+{
+	if(TOP==MAX-1)
+	{
+		printf("Stack is full(overflow)...");
+		return;
+	}
+	TOP++;
+	stack[TOP]=item;
+	printf("%d inserted successfully\n",item);
+}
+
+void Delete(int stack[])
+{
+	int deletedItem;
+	if(TOP==-1)
+	{
+		printf("Stack is empty...");
+		return;
+	}
+	deletedItem=stack[TOP];
+	TOP--;
+	printf("%d deleted successfully...",deletedItem);
+	return;
+}
